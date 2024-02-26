b7f08626318b369c6cdefe736b435d43575b78e6
Yaakov Saxon <ysaxon@nabancard.com>
2024-02-26 12:44:48 -0500

    add tests

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ if [ -f "CMakeLists.txt" ]; then
     mkdir -p build_cmake && cd build_cmake
     cmake  .. -DCMAKE_BUILD_TYPE=Debug
     make
+    ctest .. --output-on-failure
     cd ..
 else
     echo "Please run this from the project root directory."
