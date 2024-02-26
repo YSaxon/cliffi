f508038ddd442cdcb1e9b87bfc99a564f41001a4
Yaakov Saxon <ysaxon@nabancard.com>
2024-02-26 12:57:46 -0500

    imperfectly implemented --help

--- main.c
+++ main.c
@@ -10,8 +10,29 @@
 
 
 int main(int argc, char* argv[]) {
-    if (argc < 4) {
-        fprintf(stderr, "Usage: %s <library> <return_type> <function_name> [<args>...]\n", argv[0]);
+    if (strcmp(argv[1], "--help") == 0) {
+        printf("Usage: %s <library> <return_type> <function_name> [<args>...]\n", argv[0]);
+        printf("  [--help]         Print this help message\n");
+        printf("  <library>        The path to the shared library containing the function to invoke\n"
+               "                   or the name of the library if it is in the system path\n");
+        printf("  <return_type>    The type of the return value of the function to invoke\n"
+               "                   v for void, i for int, s for string, etc\n");
+        printf("  <function_name>  The name of the function to invoke\n");
+        printf("  [typeflag] <arg> The argument values to pass to the function\n"
+               "                   Types will be inferred if not prefixed with flags\n"
+               "                   Flags look like -i for int, -s for string, etc\n");
+        printf("\n"
+               "  Examples:\n"
+               "         %s libexample.so i addints 3 4\n", argv[0]);
+        printf("         %s path/to/libexample.so v dofoo\n", argv[0]);
+        printf("         %s ./libexample.so s concatstrings -s hello -s world\n", argv[0]);
+        printf("         %s libexample.so s concatstrings hello world\n", argv[0]);
+        printf("         %s libexample.so d multdoubles -d 1.5 1.5d\n", argv[0]);
+        printf("         %s libc.so i printf 'Here is a number: %%.3f' 4.5", argv[0]);
+        return 0;
+    }
+    else if (argc < 4) {
+        fprintf(stderr, "Usage: %s [--help] <library> <return_type> <function_name> [[-typeflag] <arg>...]\n", argv[0]);
         return 1;
     }
 
