a467f16a69788963a6e2881ef1965145b0eee574
Yaakov Saxon <ysaxon@gmail.com>
2024-03-12 15:32:57 -0400

    add android api,other profile improvements

--- profile_script.sh
+++ profile_script.sh
@@ -13,25 +13,37 @@ PROFILE_PATH="$HOME/.conan2/profiles/build"
 # fi
 
 # # Extract the architecture from the CC environment variable
-# ARCH=$(basename ${CC} | cut -d '-' -f 1)
-# OS=$(basename ${CC} | rev | cut -d '-' -f 3 | rev)
-# COMPILER=$(basename ${CC} | rev | cut -d '-' -f 1 | rev)
-
-
-# # sed in place the arch, os, and compiler
-# #if powerpc is in arch, replace with ppc
-# ARCH=$(echo ${ARCH} | sed -e 's/powerpc/ppc/' -e 's/i686/x86/' -e 's/i386/x86/')
-
-# #capitalize the first letter of the os
-# OS=$(echo ${OS} | sed -e 's/\b\(.\)/\u\1/')
-
-
-# cat $CMAKE_TOOLCHAIN_FILE | grep -oP 'CMAKE_SYSTEM_NAME \K\w+' | xargs -I {} echo "OS: {}"
-# cat $CMAKE_TOOLCHAIN_FILE | grep -oP 'CMAKE_SYSTEM_PROCESSOR \K\w+' | xargs -I {} echo "ARCH: {}"
-
-OS=$(cat $CMAKE_TOOLCHAIN_FILE | grep -oP 'CMAKE_SYSTEM_NAME \K\w+')
-ARCH=$(cat $CMAKE_TOOLCHAIN_FILE | grep -oP 'CMAKE_SYSTEM_PROCESSOR \K\w+')
+ARCH=$(basename ${CC} | cut -d '-' -f 1)
+OS=$(basename ${CC} | rev | cut -d '-' -f 3 | rev)
 COMPILER=$(basename ${CC} | rev | cut -d '-' -f 1 | rev)
+
+
+# sed in place the arch, os, and compiler
+#if powerpc is in arch, replace with ppc
+
+#capitalize the first letter of the os
+
+#if no commas in CC, then use the CMAKE_TOOLCHAIN_FILE
+if [[ $CC != *","* ]]; then
+    ARCH=$(cat $CMAKE_TOOLCHAIN_FILE | grep -oP 'CMAKE_SYSTEM_PROCESSOR \K\w+')
+    OS=$(cat $CMAKE_TOOLCHAIN_FILE | grep -oP 'CMAKE_SYSTEM_NAME \K\w+')
+else
+    ARCH=$(basename ${CC} | cut -d '-' -f 1)
+    OS=$(basename ${CC} | rev | cut -d '-' -f 3 | rev)
+    OS=$(echo ${OS} | sed -e 's/\b\(.\)/\u\1/')
+    COMPILER=$(basename ${CC} | rev | cut -d '-' -f 1 | rev)
+fi
+
+# allowed archs are ['x86', 'x86_64', 'ppc32be', 'ppc32', 'ppc64le', 'ppc64', 'armv4', 'armv4i', 'armv5el', 'armv5hf', 'armv6', 'armv7', 'armv7hf', 'armv7s', 'armv7k', 'armv8', 'armv8_32', 'armv8.3', 'arm64ec', 'sparc', 'sparcv9', 'mips', 'mips64', 'avr', 's390', 's390x', 'asm.js', 'wasm', 'sh4le', 'e2k-v2', 'e2k-v3', 'e2k-v4', 'e2k-v5', 'e2k-v6', 'e2k-v7', 'riscv64', 'riscv32', 'xtensalx6', 'xtensalx106', 'xtensalx7']
+
+ARCH=$(echo ${ARCH} | sed -e 's/powerpc/ppc/' -e 's/i686/x86/' -e 's/i386/x86/' -e 's/aarch64/armv8/' -e 's/extensa/extensalx6') #not sure about extensa but worth a try
+
+
+# if there is a var ANDROID_API
+if [ -n "${ANDROID_API}" ]; then
+    OS="Android"
+fi
+
 
 echo "ARCH: $ARCH"
 echo "OS: $OS"
@@ -49,6 +61,14 @@ fi
 sed -i "s/arch=.*\$/arch=$ARCH/" "$PROFILE_PATH"
 sed -i "s/compiler=.*\$/compiler=$COMPILER/" "$PROFILE_PATH"
 sed -i "s/os=.*\$/os=$OS/" "$PROFILE_PATH"
+
+if [ -n "${ANDROID_API}" ]; then
+    echo "Setting Android API level to $ANDROID_API"
+    echo "os.api_level=$ANDROID_API" >> "$PROFILE_PATH"
+elif [ "$OS" == "Android" ]; then
+    echo "Setting Android API level to $ANDROID_API (default)"
+    echo "os.api_level=21" >> "$PROFILE_PATH"
+fi
 
 # Update the [buildenv] section in the Conan profile
 {
