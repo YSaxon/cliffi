9c3f3f8e2effdec2e99ec3361af54f2df253d5ba
Yaakov Saxon <ysaxon@nabancard.com>
2024-03-13 17:52:49 -0400

    build on android without testing (since cant test without emulator)

---
+++ dockcross_buildonly.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+./profile_script.sh
+conan install . --output-folder=build --build=missing -pr build
+./update_cmake.sh
+mkdir -p build
+rm -rf build/* CMakeCache.txt CMakeFiles
+cd build
+cmake ..
+make
