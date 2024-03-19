b7f8c914dcaf6fe69d152f84e6c2ae52273444ba
Yaakov Saxon <ysaxon@nabancard.com>
2024-03-19 10:31:59 -0400

    increment minor version

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #endif
 
 const char* NAME = "cliffi";
-const char* VERSION = "0.6.2";
+const char* VERSION = "0.6.3";
 
 int main(int argc, char* argv[]) {
     if (argc > 1 && strcmp(argv[1], "--help") == 0) {
