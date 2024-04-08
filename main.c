e8edc982faf06d651af8a220217c69f9ba6021ab
Yaakov Saxon <ysaxon@nabancard.com>
2024-04-08 17:08:10 -0400

    stop appending the hash to the release tag

--- main.c
+++ main.c
@@ -44,7 +44,7 @@
 #endif
 
 const char* NAME = "cliffi";
-const char* VERSION = "1.0.0";
+const char* VERSION = "v1.0.1";
 const char* BASIC_USAGE_STRING = "<library> <return_typeflag> <function_name> [[-typeflag] <arg>.. [ ... <varargs>..] ]\n";
 
 sigjmp_buf jmpBuffer;
