d1844a374a51d7f3f3d20dea853c0c1285251e01
Yaakov Saxon <ysaxon@nabancard.com>
2024-04-08 12:45:52 -0400

    add test for lib globals reset after close command

--- main.c
+++ main.c
@@ -30,7 +30,7 @@
 #endif
 
 const char* NAME = "cliffi";
-const char* VERSION = "0.12.0";
+const char* VERSION = "0.12.2";
 const char* BASIC_USAGE_STRING = "<library> <return_typeflag> <function_name> [[-typeflag] <arg>.. [ ... <varargs>..] ]\n";
 
 sigjmp_buf jmpBuffer;
