8450370b5376d6e5169265b03ad67195d883734d
Yaakov Saxon <ysaxon@nabancard.com>
2024-04-09 16:14:26 -0400

    dockcross build first with asan and ubsan

--- dockcross_buildtest.sh
+++ dockcross_buildtest.sh
@@ -6,6 +6,9 @@ mkdir -p build
 rm -rf build/* CMakeCache.txt CMakeFiles
 conan install . --output-folder=build --build=missing -pr build -g=CMakeDeps
 cd build
-cmake .. -DUSE_FIND_PACKAGE=ON -DCMAKE_C_FLAGS="-Wall -Werror"
+cmake .. -DUSE_FIND_PACKAGE=ON -DCMAKE_C_FLAGS="-Wall -Werror" -DENABLE_SANITIZERS=ON
 make
 ctest --output-on-failure || ctest --rerun-failed --output-on-failure --extra-verbose
+#now build for release
+cmake .. -DUSE_FIND_PACKAGE=ON -DENABLE_SANITIZERS=OFF
+make
