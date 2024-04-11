cdc09737538c60f72d5a6b10dd4e8c5271f4ec6a
Yaakov Saxon <ysaxon@nabancard.com>
2024-04-11 16:28:21 -0400

    generate coverage in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 if [ -f "CMakeLists.txt" ]; then
     mkdir -p build_cmake && cd build_cmake
     cmake  .. -DCMAKE_BUILD_TYPE=Debug
-    make && ctest .. --output-on-failure
+    make && ctest .. --output-on-failure && \
+    lcov --capture --directory . --output-file ../coverage.info
+    genhtml ../coverage.info --output-directory ../coverage-html
     cd ..
 else
     echo "Please run this from the project root directory."
