4f67010680d18898efd0fe336532789544891f69
Yaakov Saxon <ysaxon@nabancard.com>
2024-05-03 13:08:14 -0400

    use brew sh if available when running cmake in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 if [ -f "CMakeLists.txt" ]; then
     mkdir -p build_cmake && cd build_cmake
+    if [[ -x "$(command -v brew)" ]]; then
+        brew sh -c "cmake  .. -DCMAKE_BUILD_TYPE=Debug"
+    else
     cmake  .. -DCMAKE_BUILD_TYPE=Debug
+    fi
     make && ctest .. --output-on-failure && \
     lcov --capture --directory . --output-file ../coverage.info
     genhtml ../coverage.info --output-directory ../coverage-html
