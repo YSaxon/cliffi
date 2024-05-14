355951a5ea471bd4a2505fa67733f9a981130fab
Yaakov Saxon <ysaxon@nabancard.com>
2024-05-14 11:22:30 -0400

    werror in build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 if [ -f "CMakeLists.txt" ]; then
     mkdir -p build_cmake && cd build_cmake
     if [[ -x "$(command -v brew)" ]]; then
-        brew sh -c "cmake  .. -DCMAKE_BUILD_TYPE=Debug"
+        brew sh -c "cmake  .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="-Werror""
     else
-    cmake  .. -DCMAKE_BUILD_TYPE=Debug
+    cmake  .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_FLAGS="-Werror"
     fi
     find . -name "*.gcda" -delete
     make && ctest .. --output-on-failure && \
