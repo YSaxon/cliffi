937b66cbcdaeecc05850aa71dac90304cdf2ec00
Yaakov Saxon <ysaxon@nabancard.com>
2024-06-07 16:08:23 -0400

    more fixes

--- main.c
+++ main.c
@@ -36,7 +36,7 @@
 #include "shims.h"
 
 const char* NAME = "cliffi";
-const char* VERSION = "v1.12.2";
+const char* VERSION = "v1.12.3";
 const char* BASIC_USAGE_STRING = "<library> <return_typeflag> <function_name> [[-typeflag] <arg>.. [ ... <varargs>..] ]\n";
 
 
@@ -553,30 +553,19 @@ typedef jint(*registerNatives_t)(JNIEnv *env, jclass clazz);
 
 typedef int (*JNI_OnLoadFunc)(void* vm, void* reserved);
 
-
 int init_jvm(JavaVM **p_vm, JNIEnv **p_env, size_t argc, char** argv) {
-  //https://android.googlesource.com/platform/frameworks/native/+/ce3a0a5/services/surfaceflinger/DdmConnection.cpp
-//   JavaVMOption opt[4];
-//   opt[0].optionString = "-Djava.class.path=/data/local/tmp/target-app.apk";
-//   opt[1].optionString = "-agentlib:jdwp=transport=dt_android_adb,suspend=n,server=y";
-//   opt[2].optionString = "-Djava.library.path=/data/local/tmp";
-//   opt[3].optionString = "-verbose:jni"; // may want to remove this, it's noisy
+    JavaVMOption options[argc];
+    for (size_t i = 0; i < argc; i++) {
+        options[i].optionString = argv[i];
+    }
 
-JavaVMOption[argc] options = {0};
-for (int i = 0; i < argc; i++) {
-    options[i].optionString = argv[i];
-}
+    JavaVMInitArgs args;
+    args.version = JNI_VERSION_1_6;
+    args.options = options;
+    args.nOptions = argc;
+    args.ignoreUnrecognized = JNI_FALSE;
 
-  // Add this option if you're hacking stuff and need it, not normally required
-  // opt[4].optionString = "-Xno_sig_chain"; // may not be require prior to ART vm, may even cause issues for DVM
-
-  JavaVMInitArgs args;
-  args.version = JNI_VERSION_1_6;
-  args.options = opt;
-  args.nOptions = 4; // Uptick this to 5, it will pass in the no_sig_chain option
-  args.ignoreUnrecognized = JNI_FALSE;
-
-  void *libdvm_dso = dlopen("libdvm.so", RTLD_NOW);
+    void *libdvm_dso = dlopen("libdvm.so", RTLD_NOW);
   void *libandroid_runtime_dso = dlopen("libandroid_runtime.so", RTLD_NOW);
 
   if (!libdvm_dso) {
@@ -584,28 +573,25 @@ for (int i = 0; i < argc; i++) {
   }
 
   if (!libdvm_dso || !libandroid_runtime_dso) {
-    return -1;
-  }
-
-  JNI_CreateJavaVM_t JNI_CreateJavaVM;
-  JNI_CreateJavaVM = (JNI_CreateJavaVM_t) dlsym(libdvm_dso, "JNI_CreateJavaVM");
-  if (!JNI_CreateJavaVM) {
-    return -2;
-  }
-
-  registerNatives_t registerNatives;
-  registerNatives = (registerNatives_t) dlsym(libandroid_runtime_dso, "Java_com_android_internal_util_WithFramework_registerNatives");
-  if (!registerNatives) {
-    // Attempt non-legacy version
-    registerNatives = (registerNatives_t) dlsym(libandroid_runtime_dso, "registerFrameworkNatives");
-    if(!registerNatives) {
-      return -3;
+        return -1;
     }
-  }
 
-  if (JNI_CreateJavaVM(&(*p_vm), &(*p_env), &args)) {
-    return -4;
-  }
+    JNI_CreateJavaVM_t JNI_CreateJavaVM = (JNI_CreateJavaVM_t) dlsym(libdvm_dso, "JNI_CreateJavaVM");
+    if (!JNI_CreateJavaVM) {
+        return -2;
+    }
+
+    registerNatives_t registerNatives = (registerNatives_t) dlsym(libandroid_runtime_dso, "Java_com_android_internal_util_WithFramework_registerNatives");
+    if (!registerNatives) {
+        registerNatives = (registerNatives_t) dlsym(libandroid_runtime_dso, "registerFrameworkNatives");
+        if (!registerNatives) {
+            return -3;
+        }
+    }
+
+    if (JNI_CreateJavaVM(p_vm, p_env, &args)) {
+        return -4;
+    }
 
   if (registerNatives(*p_env, 0)) {
     return -5;
@@ -629,34 +615,33 @@ void parseInitJNI(char* initJNICommand) {
     char* envStr = argv[2];
     void* lib_handle = getOrLoadLibrary(libraryName);
 
-    JNI_OnLoadFunc onLoadFunc = dlsym(lib_handle, "JNI_OnLoad");
-    if(onLoadFunc == NULL) {
+    JNI_OnLoadFunc onLoadFunc = (JNI_OnLoadFunc) dlsym(lib_handle, "JNI_OnLoad");
+    if (onLoadFunc == NULL) {
         printf(" [!] No JNI_OnLoad found!\n");
-        return -1;
+        return;
     }
 
     printf(" [+] Initializing JavaVM Instance\n");
     JavaVM *vm = NULL;
     JNIEnv *env = NULL;
 
-    int status = init_jvm(vm, env, argc - 3, argv + 3);
+    int status = init_jvm(&vm, &env, argc - 3, argv + 3);
 
     if (status == 0) {
-    printf(" [+] Initialization success (vm=%p, env=%p)\n", vm, env);
-  } else {
-    printf(" [!] Initialization failure (%i: %s)\n", status, dlerror());
-    return -1;
-  }
+        printf(" [+] Initialization success (vm=%p, env=%p)\n", vm, env);
+    } else {
+        printf(" [!] Initialization failure (%i: %s)\n", status, dlerror());
+        return;
+    }
 
-  printf(" [+] Found JNI_OnLoad, attempting to call\n");
-  // Depending on the target you likely want to not pass null
-  onLoadFunc(vm, NULL);
+    printf(" [+] Found JNI_OnLoad, attempting to call\n");
+    onLoadFunc(vm, NULL);
 
     setVar(vmStr, getPVar(vm));
     setVar(envStr, getPVar(env));
-
 }
 #endif
+
 
 
 int parseREPLCommand(char* command){
