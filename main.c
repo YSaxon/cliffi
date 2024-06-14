414f6a7d69a34a6d253be0f8c56e0436bc90de01
Yaakov Saxon <ysaxon@nabancard.com>
2024-06-14 13:48:05 -0400

    use falsoJNI instead

--- main.c
+++ main.c
@@ -553,53 +553,62 @@ typedef jint(*registerNatives_t)(JNIEnv *env, jclass clazz);
 
 typedef int (*JNI_OnLoadFunc)(void* vm, void* reserved);
 
-int init_jvm(JavaVM **p_vm, JNIEnv **p_env, size_t argc, char** argv) {
-    JavaVMOption options[argc];
-    for (size_t i = 0; i < argc; i++) {
-        options[i].optionString = argv[i];
+// int init_jvm(JavaVM **p_vm, JNIEnv **p_env, size_t argc, char** argv) {
+//     JavaVMOption options[argc];
+//     for (size_t i = 0; i < argc; i++) {
+//         options[i].optionString = argv[i];
+//     }
+
+//     JavaVMInitArgs args;
+//     args.version = JNI_VERSION_1_6;
+//     args.options = options;
+//     args.nOptions = argc;
+//     args.ignoreUnrecognized = JNI_FALSE;
+
+//     void *libdvm_dso = dlopen("libdvm.so", RTLD_NOW);
+//   void *libandroid_runtime_dso = dlopen("libandroid_runtime.so", RTLD_NOW);
+
+//   if (!libdvm_dso) {
+//     libdvm_dso = dlopen("libart.so", RTLD_NOW);
+//   }
+
+//   if (!libdvm_dso || !libandroid_runtime_dso) {
+//         return -1;
+//     }
+
+//     JNI_CreateJavaVM_t JNI_CreateJavaVM = (JNI_CreateJavaVM_t) dlsym(libdvm_dso, "JNI_CreateJavaVM");
+//     if (!JNI_CreateJavaVM) {
+//         return -2;
+//     }
+
+//     registerNatives_t registerNatives = (registerNatives_t) dlsym(libandroid_runtime_dso, "Java_com_android_internal_util_WithFramework_registerNatives");
+//     if (!registerNatives) {
+//         registerNatives = (registerNatives_t) dlsym(libandroid_runtime_dso, "registerFrameworkNatives");
+//         if (!registerNatives) {
+//             return -3;
+//         }
+//     }
+
+//     if (JNI_CreateJavaVM(p_vm, p_env, &args)) {
+//         return -4;
+//     }
+
+//   if (registerNatives(*p_env, 0)) {
+//     return -5;
+//   }
+
+//   return 0;
+// }
+
+#include "FalsoJNI.h"
+int init_jvm(JavaVM **p_vm, JNIEnv **p_env, size_t argc, char** argv){
+    if (argc > 0) {
+        printf(" [!] Warning: Arguments are not supported on this faker method\n");
+        jni_init();
     }
-
-    JavaVMInitArgs args;
-    args.version = JNI_VERSION_1_6;
-    args.options = options;
-    args.nOptions = argc;
-    args.ignoreUnrecognized = JNI_FALSE;
-
-    void *libdvm_dso = dlopen("libdvm.so", RTLD_NOW);
-  void *libandroid_runtime_dso = dlopen("libandroid_runtime.so", RTLD_NOW);
-
-  if (!libdvm_dso) {
-    libdvm_dso = dlopen("libart.so", RTLD_NOW);
-  }
-
-  if (!libdvm_dso || !libandroid_runtime_dso) {
-        return -1;
-    }
-
-    JNI_CreateJavaVM_t JNI_CreateJavaVM = (JNI_CreateJavaVM_t) dlsym(libdvm_dso, "JNI_CreateJavaVM");
-    if (!JNI_CreateJavaVM) {
-        return -2;
-    }
-
-    registerNatives_t registerNatives = (registerNatives_t) dlsym(libandroid_runtime_dso, "Java_com_android_internal_util_WithFramework_registerNatives");
-    if (!registerNatives) {
-        registerNatives = (registerNatives_t) dlsym(libandroid_runtime_dso, "registerFrameworkNatives");
-        if (!registerNatives) {
-            return -3;
-        }
-    }
-
-    if (JNI_CreateJavaVM(p_vm, p_env, &args)) {
-        return -4;
-    }
-
-  if (registerNatives(*p_env, 0)) {
-    return -5;
-  }
-
-  return 0;
+    *p_vm = jvm;
+    *p_env = jni;
 }
-
 
 void parseInitJNI(char* initJNICommand) {
     int argc;
