ece912d92eb7e31d6d6990cd6355c20244bbb65c
Yaakov Saxon <ysaxon@nabancard.com>
2025-05-19 17:18:55 -0400

    try running ctest with parallel
    parallel on adb too

--- prepare_and_test_via_adb.sh
+++ prepare_and_test_via_adb.sh
@@ -1,3 +1,3 @@
 adb push /work/build/cliffi /work/build/libcliffi_test.so /data/local/tmp
 cd /work/build; ctest
-ctest --output-on-failure || ctest --rerun-failed --output-on-failure --extra-verbose
+ctest --output-on-failure --parallel || ctest --rerun-failed --output-on-failure --extra-verbose
